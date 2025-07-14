ee27cb20f696d306d9857365785d158758fb2e4a
Sireesha Rachamalla <racsiri2420@gmail.com>
2025-07-14 21:28:34 +0530

    Update adc.c

--- adc.c
+++ adc.c
@@ -27,7 +27,7 @@ f32 Read_ADC(u8 chNo)
 
 	u16 adcVal=0;f32 eAR;
 
-  //select channel
+       //select channel
 
 	WRITEBYTE(ADCR,0,chNo);  
 
@@ -37,24 +37,24 @@ f32 Read_ADC(u8 chNo)
 
 	//wait for conversion time (10 bit conversion time: 2.44 us)
 
-  delay_us(3);
+        delay_us(3);
 
 	//wait for done bit
 
-  while(READBIT(ADDR,DONE_BIT) == 0);
+        while(READBIT(ADDR,DONE_BIT) == 0);
 
 	//stop the adc conversion
 
-  CLRBIT(ADCR,ADC_START_BIT);  
+        CLRBIT(ADCR,ADC_START_BIT);  
 
 	//collect the ADDR register content into adcVal variable
 
-  adcVal = (ADDR>>6)&0x3FF;
+        adcVal = (ADDR>>6)&0x3FF;
 
 	//eAR=((converted voltage*vref)/2^n-1);
 
 	eAR = adcVal*(3.3/1023);
 
-  return eAR;
+       return eAR;
 
 }
