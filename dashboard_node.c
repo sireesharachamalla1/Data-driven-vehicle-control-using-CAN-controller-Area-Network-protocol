354220bfe514903b7cbd85b47ec11f98c08808f2
Sireesha Rachamalla <racsiri2420@gmail.com>
2025-07-14 21:27:10 +0530

    Update dashboard_node.c

--- dashboard_node.c
+++ dashboard_node.c
@@ -40,7 +40,7 @@ void Init_Buzzer(void);
 
 // Main Function
 int main()
-	{
+{
    // int distance;
     float temp;
 
@@ -66,45 +66,45 @@ int main()
 }
 // hello,id  display
 void Show_Startup_Sequence(void)
-	{
+{
     char *msgs[] = {str1, str2, str3};
     int positions[] = {0x80 + 5, 0xC0, 0x01};
-		int i,j;
+     int i,j;
     for ( i = 0; i < 3; i++) 
-		{
+    {
         CmdLCD(positions[i]);
         for (j =0;j<strlen(msgs[i]);j++)
-			  {
+	{
             CharLCD(msgs[i][j]);
             delay_ms(150);
         }
         delay_s(1);
         if (i != 2)
-					CmdLCD(0x01);  // Clear screen for next line
+	  CmdLCD(0x01);  // Clear screen for next line
     }
  }
 
 // Temperature display
 void Display_Temperature(float temp) 
-	{
+{
     CmdLCD(0xC0);
     StrLCD((s8*)"T=");
     F32LCD(temp, 1);
     CharLCD(0xDF);
     CharLCD('C');
-  }
+}
 
 // Reverse gear display and CAN receive
 void Handle_Reverse_Mode(void)
-	{
+{
     CmdLCD(0x80);
     StrLCD((s8*)"R:MODE");
 
     if (((C1GSR >> 1) & 1) == 1)
-			{
+     {
         CAN1_Rx(&Rx_Frame);
         if (Rx_Frame.ID == 2)
-					{
+	{
             int distance = Rx_Frame.Data1;
             CmdLCD(0x80 + 7);
             StrLCD((s8*)"D=");
@@ -113,16 +113,16 @@ void Handle_Reverse_Mode(void)
             Control_LED(distance);
         }
      }
-			else
-				{
+     else
+     {
         CmdLCD(0x80 + 7);
         StrLCD((s8*)"         ");
-        }
+    }
   }
 
 // Forward gear display
 void Show_Forward_Mode(void)
-	{
+{
     CmdLCD(0x80);
     StrLCD((s8*)"F:MODE      ");
   }
@@ -134,15 +134,15 @@ void Display_Window_Status(int level)
     StrLCD((s8*)"W:");
 
     if (level == 0) 
-			{
+    {
         StrLCD((s8*)"OPEN  ");
-      } 
-		else if (level == 7) 
-			{
+    } 
+     else if (level == 7) 
+      {
         StrLCD((s8*)"CLOSE ");
       }
-		else
-			{
+      else
+      {
         F32LCD(arr[level], 1);
         CharLCD('%');
       }
@@ -150,29 +150,29 @@ void Display_Window_Status(int level)
 
 // LED control for reverse alert
 void Control_LED(int distance) 
-	 {
+{
     if (distance < MIN_DISTANCE_ALERT)
-		{
-			IOCLR0 = 1 << LED_PIN;
-		  IOSET0 = 1 << BUZZER_PIN ;  // Buzzer ON
-		}		    
+     {
+		IOCLR0 = 1 << LED_PIN;
+		IOSET0 = 1 << BUZZER_PIN ;  // Buzzer ON
+     }		    
     else
-		{
-			  IOSET0 = 1 << LED_PIN;
+     {
+	IOSET0 = 1 << LED_PIN;
         IOCLR0 = 1 << BUZZER_PIN;    //  Buzzer OFF 
-		}
-	}
+     }
+ }
 
 // LED pin setup
 void Init_LED(void) 
-	{
+{
     IODIR0 |= 1 << LED_PIN;
     IOSET0 = 1 << LED_PIN;
   } 
 
 	//  Buzzer Setup
 void Init_Buzzer(void)
-	{
+{
     IODIR0 |= 1 << BUZZER_PIN;
     IOCLR0 = 1 << BUZZER_PIN;  // Initially OFF
   }
@@ -181,7 +181,7 @@ void Init_Buzzer(void)
 	
 // CAN frame setup
 void Setup_CAN_Frame(void) 
-	{
+{
     Tx_Frame.ID = 2;
     Tx_Frame.BFV.RTR = 0;
     Tx_Frame.BFV.DLC = 4;
