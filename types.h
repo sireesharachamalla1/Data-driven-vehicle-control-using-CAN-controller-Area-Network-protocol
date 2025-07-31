598a337d0014eb98525ace2e2d852f80de30469c
Sireesha Rachamalla <racsiri2420@gmail.com>
2025-07-31 11:56:06 +0530

    Add files via upload

--- types.h
+++ types.h
@@ -1,26 +1,12 @@
-//types.h
-
-#ifndef _TYPES_H
-
-#define _TYPES_H
-
-
 typedef unsigned char u8;
-
-typedef signed char s8;
-
-typedef unsigned short u16;
-
+typedef char s8;
+typedef unsigned short int u16;
 typedef short int s16;
-
 typedef unsigned int u32;
-
 typedef int s32;
-
 typedef float f32;
-
 typedef double f64;
 
 
-#endif
+
 
